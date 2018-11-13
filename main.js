d2408113e42dd990961f93d618ef4205b56be392
brad-h <brad.hawbaker@gmail.com>
2018-11-13 07:07:14 -0600

    remove extraneous show button, change show behavior to mousedown/mouseup

--- main.js
+++ main.js
@@ -77,17 +77,19 @@ const data = [
   diminished("B D G Ab")
 ];
 
+const getNext = () => data[Math.floor(Math.random() * data.length)];
+let lookup = getNext();
 
-const app = document.getElementById("app");
-const quiz = document.getElementById("quiz");
-const show = document.getElementById("show");
-const next = document.getElementById("next");
-
-next.click(() => {
-  const rand = Math.floor(Math.random() * data.length);
-  const lookup = data[rand];
+const nextCard = () => {
+  lookup = getNext();
   quiz.innerText = lookup.name;
-  show.click(() => {
-    quiz.innerText = lookup.notes;
-  });
-});
+};
+
+const showName = () => {
+  quiz.innerText = lookup.name;
+}
+const showNotes = () => {
+  quiz.innerText = lookup.notes;
+};
+
+document.addEventListener('DOMContentLoaded', showName);
